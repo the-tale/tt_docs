2f53399add2ace1da10947aa70b12fff5e6f4331
serhii <aserhii@protonmail.com>
2019-07-10 21:02:47 +0300

    Add tabs to doc :art:

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,9 @@ extensions = ['sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.mathjax',
               'sphinx.ext.ifconfig',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode',
+              'sphinx_tabs.tabs',
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
